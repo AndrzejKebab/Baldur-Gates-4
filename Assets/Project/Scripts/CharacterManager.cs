e131f508f62805902b9406877973e6e15baf9e56
Andrzej Kebab <grzesiugora123@wp.pl>
2024-01-08 22:12:46 +0100

    Implement CreateCharacter method in CharacterManager
    - Introduce CreateCharacter method in CharacterManager to spawn characters in safe locations on the map.
    - Retrieve the MapGrid from the MapGenerator component to find a suitable spawn place.
    - Iterate through the grid to find the first available position for character placement.
    - Instantiate characters using the provided CharacterData at the determined safe spawn place.

--- Assets/Project/Scripts/CharacterManager.cs
+++ Assets/Project/Scripts/CharacterManager.cs
@@ -10,9 +10,27 @@ namespace GrzegorzGora.BaldurGate
 		private List<Character> allCharacters;
 
 
-		public void CreateCharacter(Character character)
+		public void CreateCharacter(params CharacterData[] characterDatas)
 		{
-			throw new NotImplementedException();
+			var _mapGrid = GetComponent<MapGenerator>().MapGrid;
+			var _safeSpawnPlace = Vector3.zero;
+
+			for (byte x  = 0; x < _mapGrid.GetLength(0); x++)
+			{
+				for (byte z = 0; z < _mapGrid.GetLength(1); z++)
+				{
+					if(!_mapGrid[x, z])
+					{
+						_safeSpawnPlace = new Vector3(x, 1, z);
+						break;
+					}
+				}
+			}
+
+			foreach (var characterData in characterDatas)
+			{
+				Instantiate(characterData, _safeSpawnPlace, Quaternion.identity);
+			}
 		}
 
 		public List<Character> SelectCharacters()
