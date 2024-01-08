78daaeafded4276b927609ab617497b399c5d572
Andrzej Kebab <grzesiugora123@wp.pl>
2024-01-08 20:23:54 +0100

    Implemented MapGenerator for procedural map generation
    - Add a MapGenerator class responsible for generating a map with walls and floors based on Perlin noise.
    - Use Unity Burst Compilation for optimized performance.
    - Initialize a boolean grid to represent wall and floor positions.
    - Use Perlin noise to determine whether a position should have a wall.
    - Instantiate Unity GameObjects (walls or floors) based on the generated grid.
    - Fine-tune noise threshold for wall placement.

---
+++ Assets/Project/Scripts/MapGenerator.cs
@@ -0,0 +1,81 @@
+using Unity.Burst;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace GrzegorzGora.BaldurGate
+{
+	[BurstCompile(CompileSynchronously = true)]
+	public class MapGenerator : MonoBehaviour
+	{
+		[SerializeField] private GameObject floor;
+		[SerializeField] private GameObject wall;
+		[SerializeField] private Material floorMaterial;
+		[SerializeField] private Material wallMaterial;
+		[SerializeField] private byte mapSize;
+		public bool[,] MapGrid;
+
+		private void Awake()
+		{
+			MapGrid = new bool[mapSize, mapSize];	
+		}
+
+		void Start()
+		{
+			InitializeMapGrid();
+		}
+
+		private void InitializeMapGrid()
+		{
+			for (byte x = 0; x < mapSize; x++)
+			{
+				for (byte z = 0; z < mapSize; z++)
+				{
+					MapGrid[x, z] = Noise(x, z);
+				}
+			}
+
+			GenerateMap();
+		}
+
+		private void GenerateMap()
+		{
+			for (byte x = 0; x < mapSize; x++)
+			{
+				for (byte z = 0; z < mapSize; z++)
+				{
+					GameObject _object;
+					if (MapGrid[x, z])
+					{
+						_object = Instantiate(wall, new Vector3(x, 0, z), Quaternion.identity, transform);
+					}
+					else
+					{
+						_object = Instantiate(floor, new Vector3(x, -0.5f, z), Quaternion.identity, transform);
+					}
+
+					_object.name = $"X: {x}, Z: {z}";
+				}
+			}
+		}
+
+		[BurstCompile]
+		private bool Noise(byte x, byte z)
+		{
+			float _scale = 10;
+			float _sampleX = x / _scale; 
+			float _sampleZ = z / _scale;
+			float2 _sampleXZ = new float2(_sampleX, _sampleZ);
+
+			var _noise = noise.snoise(_sampleXZ);
+			
+			if(_noise >= 0.25f)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
